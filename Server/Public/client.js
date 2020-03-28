d872b2a3ac05e0f0446765f282fb9c42d0c66502
Andrew Nguyen <anguyen1320@gmail.com>
2020-03-28 11:01:07 -0500

    set up the handling of operator clicked and posting inputs to server

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -2,19 +2,36 @@ $(document).ready(init);
 
 function init() {
   console.log("JQ");
-  $(".js-new-form-submit").on("submit", ".js-btn-equal", submitInputs);
-  $(".js-btn-clear").on("click", clearInputs);
+  $(".js-btn-data-equals").on("click", submitInputs);
+
+  $(".js-btn-data-operation").on("click", function(e) {
+    let oper = e.target.innerHTML;
+    console.log(oper);
+
+    handleOperator(oper);
+  });
+
+  $(".js-btn-data-clear").on("click", clearInputs);
 }
 
+let num1;
+let num2;
+let operator;
+let total;
 //
 //EVENT HANDLERS
 // -------------
 
+function handleOperator(oper) {
+  operator = oper;
+}
+
 function submitInputs(event) {
   event.preventDefault();
   const newInput = {
-    num1: $(".js-input-number1").val(),
-    num2: $(".js-input-number2").val()
+    num1: parseFloat($(".js-input-number1").val()),
+    num2: parseFloat($(".js-input-number2").val()),
+    operator: operator
   };
 
   postInput(newInput);
