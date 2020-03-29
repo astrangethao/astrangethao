0349d2b8ea2913e82cebd8eea92f41b23489816a
Andrew Nguyen <anguyen1320@gmail.com>
2020-03-29 10:51:21 -0500

    completed basemode

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -1,45 +1,33 @@
+let operator;
+let history;
+
 $(document).ready(init);
 
 function init() {
-  console.log("JQ");
   $(".js-btn-data-equals").on("click", submitInputs);
-
   $(".js-btn-data-operation").on("click", function(event) {
-    let oper = event.target.innerHTML;
-    console.log(oper);
-
-    handleOperator(oper);
+    operator = event.target.innerHTML;
   });
-
   $(".js-btn-data-clear").on("click", clearInputs);
 }
 
-let operator;
-let total;
 //
 //EVENT HANDLERS
 // -------------
-
-function handleOperator(oper) {
-  operator = oper;
-}
-
-function submitInputs(event) {
-  event.preventDefault();
-  const newInput = {
-    num1: parseFloat($(".js-input-number1").val()),
-    num2: parseFloat($(".js-input-number2").val()),
-    operator: operator
-  };
-
-  clearInputs();
-
-  postInput(newInput);
-}
-
 function clearInputs() {
   $(".js-input-number1").val("");
   $(".js-input-number2").val("");
+}
+
+function submitInputs() {
+  const newInput = {
+    num1: $(".js-input-number1").val(),
+    num2: $(".js-input-number2").val(),
+    operator: operator,
+    total: ""
+  };
+
+  postInput(newInput);
 }
 
 //
@@ -47,7 +35,6 @@ function clearInputs() {
 // -----------------------------
 
 function postInput(inputData) {
-  console.log(inputData);
   $.ajax({
     method: "POST",
     url: "/inputs",
@@ -55,29 +42,49 @@ function postInput(inputData) {
   })
     .then(response => {
       console.log(response);
-      getTotal();
-    })
-    .catch(err => {});
-}
-
-function getTotal() {
-  $.ajax({
-    type: "GET",
-    url: "/total"
-  })
-    .then(response => {
-      total = response.total;
-      console.log(total);
+      getHistory();
     })
     .catch(err => {
+      console.log(err);
       console.warn("Whoops! Something went wrong in the get request!");
     });
 }
 
-console.log(getTotal());
+function getHistory() {
+  $.ajax({
+    type: "GET",
+    url: "/inputs"
+  })
+    .then(response => {
+      history = response;
+      console.log(history);
+      render();
+    })
+    .catch(err => {
+      console.log(err);
+      console.warn("Whoops! Something went wrong in the get request!");
+    });
+}
 
 //
 // RENDER
 // -------
 
-function render() {}
+function render() {
+  console.log("render");
+  $(".js-list-record").empty();
+  for (let i = 0; i < history.length; i++) {
+    let result = history[i];
+    $(".js-list-record").append(`
+    <li>${result.num1} ${result.operator} ${result.num2} = ${result.total}</li>
+    `);
+  }
+
+  const lastIndex = history[history.length - 1];
+  if (lastIndex != undefined) {
+    $(".js-expression-result").empty();
+    $(".js-expression-result").append(`
+    <h2> ${lastIndex.total} </h2>
+    `);
+  }
+}
--- Server/server.js
+++ Server/server.js
@@ -1,55 +1,40 @@
 const express = require("express");
-const app = express();
-const PORT = 3000;
 const bodyParser = require("body-parser");
 const inputs = require("./modules/inputs");
 
-let operator;
-let num1;
-let num2;
-let total;
+const app = express();
+const PORT = 3000;
 
 app.use(express.static("server/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT} `);
-});
 
 app.get("/inputs", (req, res) => {
   res.send(inputs);
 });
 
 app.post("/inputs", (req, res) => {
-  let newInput = req.body;
-  // console.log(newInput);
+  const newInput = req.body;
 
-  for (let i = 0; i < inputs.length; i++) {
-    operator = inputs[i].operator;
-    num1 = parseFloat(inputs[i].num1);
-    num2 = parseFloat(inputs[i].num2);
+  const operator = newInput.operator;
+  const num1 = parseFloat(newInput.num1);
+  const num2 = parseFloat(newInput.num2);
 
-    console.log("operator:", operator);
-    console.log("num1:", num1);
-    console.log("num2:", num2);
-
-    if (operator == "+") {
-      total = num1 + num2;
-    } else if (operator == "-") {
-      total = num1 - num2;
-    } else if (operator == "*") {
-      total = num1 * num2;
-    } else if (operator == "/") {
-      total = num1 / num2;
-    }
+  if (operator == "+") {
+    newInput.total = num1 + num2;
+  } else if (operator == "-") {
+    newInput.total = num1 - num2;
+  } else if (operator == "*") {
+    newInput.total = num1 * num2;
+  } else if (operator == "/") {
+    newInput.total = num1 / num2;
   }
+
   inputs.push(newInput);
-  console.log("total:", total);
 
   res.sendStatus(201);
 });
 
-app.get("/total", (req, res) => {
-  res.send({ total: total });
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT} `);
 });
